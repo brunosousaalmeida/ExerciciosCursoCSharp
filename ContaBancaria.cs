0e6dacd5f953b9c17cee2e2ed5aa2b54ea3c610f
Bruno Sousa de Almeida Oliveira <bruno.almeida.developer@gmail.com>
2020-09-29 22:36:25 -0300

    Iniciando atividade unica da seção 5

---
+++ ContaBancaria.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Text;
+
+namespace ExerciciosCursoCSharp
+{
+    class ContaBancaria
+    {
+        private string _nome;
+        public int NumeroConta { get; private set; }
+        public double DepositoInicial { get; private set; }
+
+        ContaBancaria(string nome, int numeroConta, double depositoInicial)
+        {
+            _nome = nome;
+            NumeroConta = numeroConta;
+            DepositoInicial = depositoInicial;
+
+        }
+        ContaBancaria(string nome, int numeroConta)
+        {
+            _nome = nome;
+            NumeroConta = numeroConta;
+            DepositoInicial = 0;
+        }
+
+        public string Nome
+        {
+            get { return _nome; }
+            set
+            {
+                if(value != null && value.Length > 1)
+                {
+                    _nome = value;
+                }
+            }
+        }
+
+
+    }
+}
